369438d0f9e8541cdd215be28b151715ad767de4
Kirthika Venkatesan <kirthika.v@zucisystems.com>
2020-10-07 18:53:19 +0530

    First Commit

---
+++ src/sample_Package/Sample_Class.java
@@ -0,0 +1,11 @@
+package sample_Package;
+
+public class Sample_Class {
+	
+	public static void main (String args[])
+	{
+	System.out.println("Java Hello World");
+	System.out.println("Sample Program is created Successfully.....!!");
+	}
+
+}
